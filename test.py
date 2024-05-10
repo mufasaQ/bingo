efc8ba84808ca4c1ac2389843d6d267721db222e
mufasaQ <168477996+mufasaQ@users.noreply.github.com>
2024-05-10 22:42:13 +0200

    Add files via upload
    asasas

---
+++ test.py
@@ -0,0 +1,12 @@
+
+
+list_l=["hi","ich","du","haben",1,"gemeinsam","und"]
+
+
+for i in range(6):
+    if(i==0):continue
+    if(i==2):print(list_l[6])
+    if(i==2):continue
+    print(list_l[i])
+
+print("HELLO")
